63c795c541c0dd239deff8472236c1f44fd9cff6
dulma12722 <yukiho27mst@gmail.com>
2025-07-06 00:25:53 +0900

    kadai_007提出 20250706

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="kadai_007">
+  <title>連想配列を作ってキーと値を出力しよう</title>
+</head>
+<body>
+  <p>
+    <?php
+    // 連想配列に値を入力する
+    $vegetables = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    // 連想配列の値を出力する
+    print_r($vegetables);
+    ?>
+  </p>
+</body>
+</html>
+
+<!-- localhost/php-basic-kadai/kadai_007/associative-array.php -->
