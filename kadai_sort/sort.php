d188d17e8873688f690b165029bb00625396db31
dulma12722 <yukiho27mst@gmail.com>
2025-07-17 23:15:34 +0900

    kadai_sort提出 20250717

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -9,31 +9,37 @@
   <p>
     <?php
 
-    /* 昇順に並び替えのコード
     // 配列に値を格納
     $array = [15, 4, 18, 23, 10];
 
-    // 配列の要素を昇順[TRUE]に並び替え
-    sort($array);
-
-    // 配列$arrayの要素を1つずつ順番に出力する
-    // 配列名：$array、変数名：$num
-    foreach ($array as $num) {
-      echo $num . '<br>';
-    }
-    */
-
-    // if文
-    function sort_2way($order) {
+    // 関数を定義 ----------------------------------------------------------------------------
+    function sort_2way($array,$order) {
+      
       if ($order) {
-        return "昇順にソートします<br>";
+
+        echo "昇順にソートします<br>";
+        // 昇順の出力 - 配列の要素を昇順[TRUE]に並び替え
+        sort($array);
+
       } else {
-        return "降順にソートします<br>";
+
+        echo "降順にソートします<br>";
+        // 降順の出力 - 配列の要素を降順[FALSE]に並び替え
+        rsort($array);
+
+      }
+
+      // 配列$arrayの要素を1つずつ順番に出力する（配列名：$array、変数名：$num） 
+      foreach ($array as $num) {
+        echo $num . '<br>';
       }
     }
+    // ---------------------------------------------------------------------------- 関数を定義
 
-    // 1がTRUE、0がFALSE
-    echo sort_2way(1);
+    // 出力、『1』がTRUE、『0』がFALSE
+    echo sort_2way($array,1);
+    echo '<hr>';
+    echo sort_2way($array,0);   
 
     ?>
   </p>
